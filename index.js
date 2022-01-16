f9bb4963437eb77bcd422cabc9fc4d568f615f84
Riccardo Rossi <riki.redz@gmail.com>
2022-01-16 19:52:06 +0100

    changes

--- index.js
+++ index.js
@@ -2,40 +2,40 @@ const app = require('express')();
 const protobuf = require('protobufjs');
 
 async function run() {
-    const root = await protobuf.load('donkeys.proto')
-    const Donkey = root.lookupType('Donkey')
-    const Donkeys = root.lookupType('Donkeys')
+    const root = await protobuf.load('developers.proto')
+    const GetDevelopersResponse = root.lookupType('GetDevelopersResponse')
+    const Developer = GetDevelopersResponse.lookupType('Developer')
 
-    const fabio = Donkey.fromObject({
+    const fabio = Developer.fromObject({
         id: "1",
         name: "Fabio",
         surname: "Angelilli",
         age: 27,
+        platform: "iOS",
         bald: true
     })
 
-    const rick = Donkey.fromObject({
+    const rick = Developer.fromObject({
         id: "2",
         name: "Riccardo",
         surname: "Rossi",
+        platform: "React",
         age: 27,
         bald: false
     })
 
-    const antonio = Donkey.fromObject({
-        id: "3",
-        name: "Antonio",
-        surname: "Epifani",
-        age: 23,
-        bald: false
+    const message = GetDevelopersResponse.fromObject({
+        developers: [fabio, rick]
     })
 
-    const donkeysMsg = Donkeys.fromObject({
-        donkeys: [fabio, rick, antonio]
-    })
+    app.get('/developers', (_, res) => {
+        const encodedMessage = GetDevelopersResponse.encode(message).finish()
 
-    app.get('/donkeys', (req, res) => {
-        res.status(200).send(Donkeys.encode(donkeysMsg).finish())
+        const payloadMessage = GetDevelopersResponse.decode(encodedMessage)
+        const payload = GetDevelopersResponse.toObject(payloadMessage)
+        console.log(payload)
+
+        res.status(200).send(encodedMessage)
     })
     
     app.listen(8080)
